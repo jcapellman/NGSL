8c3f890d76c393241bf17cbc4243871d634559d0
Jarred Capellman <jcapellman@hotmail.com>
2021-07-22 19:45:14 -0500

    Added: Unit Tests project

---
+++ NGSL.UnitTests/MainTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NGSL.UnitTests
+{
+    [TestClass]
+    public class MainTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
