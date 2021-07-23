0ba24318829c5178f91f2e38d62ff632b0f8da8f
Jarred Capellman <jcapellman@hotmail.com>
2021-07-23 17:20:25 -0500

    Added: Initial work on the NGSLEngine

---
+++ NGSL.lib/NGSLEngine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+
+using Lextm.SharpSnmpLib;
+using Lextm.SharpSnmpLib.Messaging;
+
+using Microsoft.Extensions.Logging;
+
+namespace NGSL.lib
+{
+    public class NGSLEngine
+    {
+        private Discoverer discoverer;
+
+        private ILogger? _logger;
+
+        public NGSLEngine(ILogger? logger = null)
+        {
+            _logger = logger;
+
+            discoverer = new Discoverer();
+        }
+
+        public async void Start()
+        {
+            discoverer.AgentFound -= Discoverer_AgentFound;
+            discoverer.AgentFound += Discoverer_AgentFound;
+
+            await discoverer.DiscoverAsync(VersionCode.V1, new IPEndPoint(IPAddress.Broadcast, 161), new OctetString("public"), 6000);
+        }
+
+        private void Discoverer_AgentFound(object? sender, AgentFoundEventArgs e)
+        {
+            // TODO: Log and handle
+        }
+    }
+}
