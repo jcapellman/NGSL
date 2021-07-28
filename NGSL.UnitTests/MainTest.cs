cb2a60917a60482fec552e1812a04e757b29ef9a
Jarred Capellman <jcapellman@hotmail.com>
2021-07-28 19:52:52 -0400

    Added: Unit Tests

--- NGSL.UnitTests/MainTest.cs
+++ NGSL.UnitTests/MainTest.cs
@@ -1,13 +1,29 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using NGSL.lib;
+using NLog;
+
 namespace NGSL.UnitTests
 {
     [TestClass]
     public class MainTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void NGSLEngine_NullLogger()
         {
+            var engine = new NGSLEngine(null);
+
+            Assert.IsNotNull(engine);
+        }
+
+        [TestMethod]
+        public void NGSLEngine_NotNullLogger()
+        {
+            var logger = LogManager.GetCurrentClassLogger();
+
+            var engine = new NGSLEngine();
+
+            Assert.IsNotNull(engine);
         }
     }
 }
