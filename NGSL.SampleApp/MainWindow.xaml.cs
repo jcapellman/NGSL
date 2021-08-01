08748c561b0ae2d1cf79eab6c13ff57e46ec170d
Jarred Capellman <jcapellman@hotmail.com>
2021-08-01 15:01:53 -0500

    Added: WPF Sample Application

---
+++ NGSL.SampleApp/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+
+namespace NGSL.SampleApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var engine = new lib.NGSLEngine();
+
+            engine.OnNewAssetDiscovered += Engine_OnNewAssetDiscovered;
+
+            engine.Start();
+        }
+
+        private void Engine_OnNewAssetDiscovered(object? sender, lib.Objects.NGSLAsset e)
+        {
+            txtBxMain.Text += $"{e}{Environment.NewLine}";
+        }
+    }
+}
